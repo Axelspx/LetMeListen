1d43c78a917c9f2a7626d4b85ef42c6ebe4b0b07
AxelSPX <ax.spx@pm.me>
2026-02-28 03:10:00 +0000

    Keybind session mute excluding foreground window pid and relevant children pids, working.

--- hotkey.py
+++
@@ -1,4 +0,0 @@
-
-
-mute_hotkey: list= ["alt", "x"]
-
--- main.py
+++ main.py
@@ -1,27 +1,71 @@
-#import pycaw
-#import win32
-#import win32process
-#import win32gui
+from pycaw.pycaw import AudioUtilities
+import win32process
+import win32gui
 import keyboard
 import time
+import comtypes
+from typing import Any
+import psutil
 
-from audio import mute_session_toggle
-from window import get_foreground
-from hotkey import mute_hotkey
+comtypes.CoInitialize()
 
-def mute_focus_window():
+mute_hotkey: list= ["alt", "x"]
+muted: bool= False
+
+## MAIN APP CONTROL ##
+
+def toggle_mute():
+    global muted
     print(f"Keyboard hotkey pressed")
-    mute_session_toggle(get_foreground())
+    mute_sessions_toggle()
+    muted = not muted
     return
 
-#def keyboard_hook(event):
-    #print(event.name)
+
+
+## WINDOW ##
+
+def get_foreground() -> list:
+    # gets foreground window HWND, then gets thread and process identifier of window origin process
+    fg_hwnd = win32gui.GetForegroundWindow()
+    _, fg_pid = win32process.GetWindowThreadProcessId(fg_hwnd) #thread identifier(_) not needed
+    child_pids = get_foreground_children(fg_pid)
+    fg_title = win32gui.GetWindowText(fg_hwnd)
+    print(f" - Foreground window:\n   {fg_title}\n - Relevant PIDs:\n   {[fg_pid]+child_pids}")
+    return [fg_pid] + child_pids
+
+def get_foreground_children(fg_pid):
+    try:
+        process = psutil.Process(fg_pid)
+        children = process.children(recursive=True) #recursive gets all descendants of process; children, grandchildren etc
+        return [child.pid for child in children]
+    except Exception:
+        return []
+
+
+
+## AUDIO ##
+
+def get_audio_sessions() -> list:
+    comtypes.CoInitialize()
+    sessions = AudioUtilities.GetAllSessions()
+    return sessions
+
+def mute_sessions_toggle() -> Any:
+    pids = get_foreground()
+    sessions = get_audio_sessions()
+    for session in sessions:
+        if not muted and session.Process and session.Process.pid not in pids:
+            print(f"Muting background process - PID:{session.Process.pid}")
+            session.SimpleAudioVolume.SetMute(1, None)
+        if muted and session.Process:
+            print(f"Unmuting background process: {session.Process.pid}")
+            session.SimpleAudioVolume.SetMute(0, None)
+
+
 
 
 if __name__ == "__main__":
-
-    #keyboard.hook(keyboard_hook)
-    keyboard.add_hotkey("+".join(mute_hotkey), mute_focus_window)
-
+    keyboard.add_hotkey("+".join(mute_hotkey), toggle_mute)
     while True:
         time.sleep(0.1)
